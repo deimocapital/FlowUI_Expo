49c8e82fc91461ba9c929c9354a6a4f18d503bfb
Diego Isunza <contacto.diegoisunza@gmail.com>
2023-07-17 08:38:39 -0700

    flow balance not static

--- src/screens/LogIn/index.tsx
+++ src/screens/LogIn/index.tsx
@@ -2,17 +2,20 @@ import React, {useState, useEffect, useContext} from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import * as Clipboard from 'expo-clipboard';
 import { UserContext } from '../../context/UserContext';
-
+import { CurrentUser } from "@onflow/typedefs";
+import { useAccount } from "../../hooks/useAccount";
 import { addUser } from '../../utils/UserApi';
 import Entypo from 'react-native-vector-icons/Entypo';
 import styles from './styles';
 import Button from '../../components/Button';
+
 import "../../../flow/config";
 import * as fcl from "@onflow/fcl/dist/fcl-react-native";
 
 const LogIn = () => {
-  const [usuario, setUsuario] = useState({loggedIn: null});
+  const [usuario, setUsuario] = useState<CurrentUser | null>({loggedIn: null});
   const { setUser } = useContext(UserContext);
+  const { account, error } = useAccount(usuario?.addr);
 
   const imageAddress = 'https://classicalarchitectures.weebly.com/uploads/1/2/6/8/126890479/sculptural-home-plays-volumes-curvy-roofline-1-exterior_orig.jpg';
 
@@ -20,11 +23,12 @@ const LogIn = () => {
   const { services, isLoading, authenticateService } = fcl.useServiceDiscovery({ fcl });
 
   if(usuario.loggedIn){
+    
     setUser(usuario.addr);
     addUser(usuario.addr);
   };
   
-
+  
   const logout = () => {
     fcl.unauthenticate();
   };
@@ -53,7 +57,7 @@ const LogIn = () => {
                 {services.map((service, index) => (
                   <Image key={index} style={styles.imageProviders} source={{ uri: service?.provider?.icon }} />
                 ))}
-               <Text style={[styles.message, {alignSelf:'center'}]}>210 $FLOW</Text>
+               <Text style={[styles.message, {alignSelf:'center'}]}>{(account?.balance / 2 ** 10).toFixed(2)} $FLOW</Text>
               </View>
             </View>
           </View>
